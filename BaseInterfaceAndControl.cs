78cd4726d2e5ac774e0ce8d2b7f8f4a8ffad1552
mmeents <mattmeents@gmail.com>
2014-04-28 11:48:31 -0400

    Version 1
    COM C# component to get content at url and return the value as a string.

---
+++ BaseInterfaceAndControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace GoGetURL {
+
+  [ComVisible(true)]
+  [Guid("CCCFB659-D65B-4B41-B0F7-9BEB83329671")]  
+  public interface IGoGetURL{
+    String GetContentsAtURLString(String sURL);
+  }
+
+  [ComVisible(true)]  
+  [Guid("ECD0A309-F898-4F9E-9054-6ECE3672DCD2")]
+  [ClassInterface(ClassInterfaceType.AutoDual)]
+  public class GoGetURL : IGoGetURL {
+    public String GetContentsAtURLString(String sURL) {
+      String theURL = sURL;
+      String theResult = "";
+      HttpWebRequest webReq = WebRequest.Create(theURL) as HttpWebRequest;
+      HttpWebResponse webResponse = webReq.GetResponse() as HttpWebResponse;
+      if (webResponse.StatusCode != HttpStatusCode.OK) {
+        theResult = String.Format("Error (HTTP {0}: {1}).", webResponse.StatusCode, webResponse.StatusDescription);
+      } else {
+        Stream dataStream = webResponse.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+        theResult = reader.ReadToEnd();
+      }
+      return theResult;
+    }
+  }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("GoGetURL")]
+[assembly: AssemblyDescription("COM Component exposes function to get content at url")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SolutionStudio.net")]
+[assembly: AssemblyProduct("GoGetURL")]
+[assembly: AssemblyCopyright("2014 SolutionStudio.net")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("42fd84d1-07d5-47e2-9e30-aba8e73b1469")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
